4fc899f745b470b9b22d97759db95df75c9f5f21
Md. Mazharul Islam <mazharulislam611@gmail.com>
2021-08-20 22:54:29 +0600

    update

--- src/components/AddEmployee/AddEmployee.js
+++ src/components/AddEmployee/AddEmployee.js
@@ -3,6 +3,7 @@ import { Container, Form, Button } from 'react-bootstrap';
 import MainMenu from '../MainMenu/MainMenu';
 
 const AddEmployee = () => {
+
     return (
         <div>
             <MainMenu></MainMenu>
@@ -10,28 +11,23 @@ const AddEmployee = () => {
                 <h3 className="text-center pt-3">Added Employee : </h3>
                 <Form>
                     <Form.Group className="mb-3" controlId="firstName">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control type="text" placeholder="First Name" />
+                        <Form.Control type="text" placeholder="First Name" required />
                     </Form.Group>
-                    
                     <Form.Group className="mb-3" controlId="lastName">
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="text" placeholder="Last Name" />
+                        <Form.Control type="text" placeholder="Last Name" required />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
+                        <Form.Control type="email" placeholder="Enter email" required />
+                        {/* <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
-                        </Form.Text>
+                        </Form.Text> */}
                     </Form.Group>
                     
                     <Form.Group controlId="formFile" className="mb-3">
-                        <Form.Label>Default file input example</Form.Label>
                         <Form.Control type="file" />
                     </Form.Group>
                     <Button variant="primary" type="submit">
-                        Submit
+                        Add
                     </Button>
                 </Form>
             </Container>
